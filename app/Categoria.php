29e26f5d145030aeeaa9f2d7d27504bfc070c870
jaime angulo <jaime845@hotmail.com>
2020-04-12 12:56:04 -0500

    se creo un modelo de la tabla categoria

---
+++ app/Categoria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    //indicar la modelo con que tabla trabaja
+    protected $table = 'categorias';
+    protected $primarykey = 'id';
+    protected $fillable =['nombre','descripcion','condicion']
+}
